d615b6100b1a438e6b70fe39e1404f17d3874770
User07Nidhi <roy.workspace76@gmail.com>
2024-12-17 23:02:45 +0530

    Update Header.js

--- Bravit/frontend/src/components/Header.js
+++ Bravit/frontend/src/components/Header.js
@@ -1,16 +1,20 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './Header.css';
-import logo from '../medicine.png'; // Use the correct relative path
 
 const Header = () => {
   return (
     <header>
-      <div className="navbar">
-        {/* Logo Image */}
-        <Link to="/">
-          <img src={logo} alt="Bravit Logo" className="logo" />
-        </Link>
+      <div className="nav-container">
+        <div className="logo">
+          <Link to="/">
+            <img
+              src="/logo.png" /* Adjust this path if needed */
+              alt="Bravit Logo"
+              id="logo"
+            />
+          </Link>
+        </div>
         <nav>
           <ul className="nav-links">
             <li><Link to="/">Home</Link></li>
